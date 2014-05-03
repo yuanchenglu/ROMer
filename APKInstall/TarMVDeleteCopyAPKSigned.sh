f624d27117fca9605cf2b07e37158e240fbb189b
bluth653426595 <ycl_pj@163.com>
2014-05-03 12:02:45 +0800

    add sign_files_in_folder_bluth

--- APKInstall/TarMVDeleteCopyAPKSigned.sh
+++ APKInstall/TarMVDeleteCopyAPKSigned.sh
@@ -1,24 +1,24 @@
 
-APKInstall_dir=~/project/ROMer/APKInstall
-chufang_dir=~/soft/ChuFang_cn_0.179
-
-signed_dir=$chufang_dir/sign_files
 rom_old_name=精英IOCEAN_X7_MIUI_4.5.2开发版.zip
 rom_new_name=Bluth_精英IOCEAN_X7_MIUI_4.5.2.zip
 
+
+APKInstall_dir=~/project/ROMer/APKInstall
+chufang_dir=~/soft/ChuFang_cn_0.179
 
 work_dir=$chufang_dir/WORKING_
 work_old_dir=$chufang_dir/WORKING_OLD
 orignal_update_dir=$chufang_dir/original_update
 tmp_orignal_dir=$orignal_update_dir/tmp
 
+sign_files_dir=$chufang_dir/sign_files_dir
+signed_update_zip_bluth_dir=$chufang_dir/signed_update_zip_bluth
+
 chufang_scripts_dir=$chufang_dir/scripts
 zipalign=$chufang_scripts_dir/do_zipalign
 change_rom_name=$chufang_scripts_dir/change_rom_name
 deodex=$chufang_scripts_dir/do_deodex
-signed_files=$chufang_scripts_dir/signed_files
-
-signed_dir=$chufang_dir/signed_update_zip
+sign_files_in_folder_bluth=$chufang_scripts_dir/sign_files_in_folder_bluth
 
 cd $chufang_dir/
 rm -r $work_old_dir/
@@ -47,10 +47,11 @@ $change_rom_name
 
 
 cd $chufang_dir/
-zip $rom_new_name work_dir
+zip $rom_new_name $work_dir
 
 
-cp $rom_new_name $sign_dir/
-$signed_files
-cd $sign_dir/
-mv *.zip $signed_dir/
+cd $chufang_dir/
+cp $rom_new_name $sign_files_dir/
+$sign_files_in_folder_bluth
+cd $sign_files_dir/
+mv *.zip $signed_update_zip_bluth_dir/
