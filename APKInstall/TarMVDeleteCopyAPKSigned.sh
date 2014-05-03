ba18fa2be58737ac77aacdada483434bf80741f6
bluth653426595 <ycl_pj@163.com>
2014-05-03 11:38:43 +0800

    update Tar

--- APKInstall/TarMVDeleteCopyAPKSigned.sh
+++ APKInstall/TarMVDeleteCopyAPKSigned.sh
@@ -2,14 +2,14 @@
 APKInstall_dir=~/project/ROMer/APKInstall
 chufang_dir=~/soft/ChuFang_cn_0.179
 
-signed_dir=$chufang_dir/signed_files
+signed_dir=$chufang_dir/sign_files
 rom_old_name=精英IOCEAN_X7_MIUI_4.5.2开发版.zip
 rom_new_name=Bluth_精英IOCEAN_X7_MIUI_4.5.2.zip
 
 
 work_dir=$chufang_dir/WORKING_
 work_old_dir=$chufang_dir/WORKING_OLD
-orignal_update_dir=$chufang_dir/orignal_update
+orignal_update_dir=$chufang_dir/original_update
 tmp_orignal_dir=$orignal_update_dir/tmp
 
 chufang_scripts_dir=$chufang_dir/scripts
@@ -18,7 +18,7 @@ change_rom_name=$chufang_scripts_dir/change_rom_name
 deodex=$chufang_scripts_dir/do_deodex
 signed_files=$chufang_scripts_dir/signed_files
 
-
+cd $chufang_dir/
 rm -r $work_old_dir/
 mkdir $work_old_dir/
 cd $work_dir/
